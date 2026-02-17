50c33490b214182cf62a36df52ed94a867dde704
Manish Sisodia <manish.sisodia@aurigait.com>
2026-02-17 23:28:51 +0530

    change put to get

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @PutMapping("/hello")
+    @GetMapping("/hello")
     public ResponseEntity<?> helloWorld(){
         return new ResponseEntity<>("Hello World Using Jenkins", HttpStatus.OK);
     }
